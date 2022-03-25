d960ed3e8f46f70d246a23e3295598a5d7f76424
SergioPenaloza0507 <sergio.penaloa.herrera@gmail.com>
2022-03-25 11:33:27 -0500

    Session 16 finished

---
+++ Assets/Shaders/Session 16/LightingData.hlsl
@@ -0,0 +1,22 @@
+﻿void GetMainLightData_half(half3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAttenuation, out half ShadowAttenuation)
+{
+    
+    #if defined(SHADERGRAPH_PREVIEW)
+    Direction = half3(0.5, 0.5, 0);
+    Color = 1;
+    DistanceAttenuation = 1;
+    ShadowAttenuation = 1;
+    #else
+    #if defined(SHADOWS_SCREEN)
+    half4 clipPos = TransformWorldToHClip(WorldPos);
+    half4 shadowCoord = ComputeScreenPos(clipPos);
+    #else
+    half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAttenuation = mainLight.distanceAttenuation;
+    ShadowAttenuation = mainLight.shadowAttenuation;
+    #endif
+}
